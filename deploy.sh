bfed8d70294b650a045f0665d7726106abae1e5e
La Mantia Jordan <lamantiajordan@gmail.com>
2021-03-08 16:26:32 +0100

    master: deploy script written

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+DIR='./functions'
+for FUNCTION in "$DIR"/*
+do
+    cd $FUNCTION
+    
+    function_name=$(echo $FUNCTION | rev | cut -d '/' -f 1 | rev)
+    GCP_PROJECT=jordan-playground-306709 
+    
+    gcloud functions deploy $function_name \
+        --entry-point $function_name \
+        --runtime nodejs10 \
+        --trigger-http \
+        --region europe-west1 \
+        --allow-unauthenticated \
+        --project $GCP_PROJECT
+
+    cd ../..
+done
